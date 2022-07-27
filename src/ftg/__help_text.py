78fbbb50cdde4407c3477ad016d962250a3b8f90
watermelonwolverine <29666253+watermelonwolverine@users.noreply.github.com>
2022-07-27 13:13:56 +0200

    Improved help help_dialog.py and __help_text.py

--- src/ftg/__help_text.py
+++ src/ftg/__help_text.py
@@ -1,113 +1,253 @@
-help_text = \
-    str('# About\n'
-        '\n'
-        'The primary purpose of this program is to tag files by adding the tags to the name of the file.\n'
-        '\n'
-        'The use case it is designed for is the organization of rpg map hoards and other media files a DM comes in contact with.\n'
-        '\n'
-        'Anyone who every comes into contact with this task will quickly find that no folder structure will every be the right one.\n'
-        '\n'
-        '## Example\n'
-        '\n'
-        'To add the tags `tag1`, `tag2` and `tag3` to the file `some_map.png` it would be renamed to `SOME-MAP;tag1_tag2_tag3.png\n'
-        '`\n'
-        '\n'
-        '# Where to Start\n'
-        '\n'
-        'If you are a new user who is planning to use this program read about its [limitations](#Limitations) first.\n'
-        '\n'
-        'Then go to [Install](#Install)\n'
-        '\n'
-        'Then go to [Usage](#Usage)\n'
-        '\n'
-        'If you want to understand the decisions that lead to this mess go to [Why Filenames?](#Why Filenames?)\n'
-        '\n'
-        'If you think that putting the tags into the filenames is a bad solution go to [Alternatives](#Alternatives)\n'
-        '\n'
-        '# Limitations\n'
-        '\n'
-        'Putting the tags into the filenames is limited by the maximum path length and maximum file length.\n'
-        'Windows, Linux, GoogleDrive, OneDrive, Dropbox, etc. all have different restraints\n'
-        'on those.\n'
-        '\n'
-        '- Linux has a maximum filename length of 255 characters for most filesystems (including EXT4), and a maximum path of\n'
-        '  4096\n'
-        '  characters. [link](https://unix.stackexchange.com/questions/32795/what-is-the-maximum-allowed-filename-and-folder-size-with-ecryptfs)\n'
-        '- Without Long Paths enabled Windows 10 has a maximum path length of\n'
-        '  260 [link](https://docs.microsoft.com/en-us/windows/win32/fileio/maximum-file-path-limitation?tabs=registry)\n'
-        '- With Long Paths enabled Windows 10 has a maximum path length of\n'
-        '  32,767 [link](https://docs.microsoft.com/en-us/windows/win32/fileio/maximum-file-path-limitation?tabs=registry#:~:text=path%20length%20of-,32%2C767%20characters,-.%20This%20type%20of)\n'
-        '\n'
-        'If you intend to use this tool you should check what the systems you are planning to use\n'
-        'are supporting. Maybe even run some tests.\n'
-        '\n'
-        'Make yourself clear about these limitations before using this tool as they cannot be circumvented.\n'
-        '\n'
-        'That said in most cases it´s pretty easy to stay below the limits unless you use very high number of long tags.\n'
-        '\n'
-        '# Why Filenames?\n'
-        '\n'
-        'This is the conclusion of a [reddit post](TODO)\n'
-        '\n'
-        '# Summary\n'
-        '\n'
-        '- Filenames are a good enough solution in the majority of cases.\n'
-        '- Every good enough solution is better than a perfect solution that is never finished.\n'
-        '- Every alternative to filenames I found so far either had other limitations or would require 100s of hours of work\n'
-        '  to produce a satisfying program.\n'
-        '- I don`t get paid, and I won`t spend 100s of hours implement a niche tool that only a handful of people will use.\n'
-        '\n'
-        '## Why Filenames are Good\n'
-        '\n'
-        '- Very verbose, can see tags with every file browser\n'
-        '- Re-using what already exists\n'
-        '    - File browser\n'
-        '    - Thumbnail cache\n'
-        '    - Search and indexing functionality\n'
-        '    - Image viewers\n'
-        '- Works in pretty much every cloud web interface\n'
-        '    - => Access your media files via tags everywhere without having to download them first\n'
-        '- Works with every type of file\n'
-        '- Tags don`t get lost on conversion\n'
-        '- Tags don`t get lost when file is moved outside the tool\n'
-        '- Tags always stay synced with file even when working on multiple machines \n'
-        '  - => No need to sync databases, metafiles etc.\n'
-        '\n'
-        '## Alternative Solutions\n'
-        '\n'
-        '### Database\n'
-        '\n'
-        '- What happens when files are moved outside the tool?\n'
-        '- Would require indexing\n'
-        '- Needs a thumbnail cache and a file browser with search functionality\n'
-        '- How to sync the database to different PC?\n'
-        '- Can`t use tags in the cloud web interface\n'
-        '- Would basically just re-implement one of the countless tagging solutions that already exist\n'
-        '\n'
-        '### Accompanying Metafiles\n'
-        '\n'
-        '- Files always have to be moved together\n'
-        '- Would require indexing\n'
-        '- Needs a thumbnail cache and a file browser with search functionality\n'
-        '- Can`t use tags in the cloud web interface\n'
-        '\n'
-        '### Metadata of the Files\n'
-        '\n'
-        '- Not supported for a lot of file types\n'
-        '- Would need to treat every file type special\n'
-        '- Would require indexing\n'
-        '- Needs a thumbnail cache and a file browser with search functionality\n'
-        '- Can`t use tags in the cloud web interface\n'
-        '\n'
-        '# Alternatives\n'
-        '\n'
-        'Originally I was going to put a list of alternatives here, but then I found too many and none really satisfied me. That why I created this tool in the first place.\n'
-        '\n'
-        'There is an abundance of tagging tools out there. If you find one that is \n'
-        '- cross-platform\n'
-        '- works with cloud sync\n'
-        '- works in the cloud web interface\n'
-        '- can easily manage 100s of tags\n'
-        '- can easily manage dozens of tags per file  \n'
-        '\n'
-        'let me know, so I can add it.')
+from typing import Dict, List
+
+from ftg.__cli_wrapper import __paths
+
+
+def __to_link__(header: str):
+    result = header.lower()
+    result = result.strip(" ?!.")
+    result.replace(" ", "-")
+    return "#" + result
+
+
+about_header = "About"
+about_link = __to_link__(about_header)
+
+where_to_start_header = "Where to Start"
+where_to_start_link = __to_link__(where_to_start_header)
+
+limitations_header = "Limitations"
+limitations_link = __to_link__(limitations_header)
+
+why_filenames_header = "Why Filenames?"
+why_filenames_link = __to_link__(why_filenames_header)
+
+alternatives_header = 'Alternatives'
+alternatives_link = __to_link__(alternatives_header)
+
+where_is_my_stuff_header = 'Where is my Stuff?'
+where_is_my_stuff_link = __to_link__(where_is_my_stuff_header)
+
+install_header = "Install"
+install_link = __to_link__(install_header)
+
+usage_header = "Usage"
+usage_link = __to_link__(usage_header)
+
+about_text = str(
+    F'# {about_header}\n'
+    '\n'
+    'The primary purpose of this program is to tag files by appending the tags to the name of the file.\n'
+    '\n'
+    'The use case it is designed for is the organization of RPG maps and other media files a DM or GM comes in contact with.\n'
+    '\n'
+    '## Example\n'
+    '\n'
+    'To add the tags \n'
+    '\n'
+    '`tag1`, `tag2` and `tag3`\n'
+    '\n'
+    'to the file `some_map.png` it would be renamed to\n'
+    '\n'
+    '`some_map;tag1_tag2_tag3.png\n'
+    '`\n'
+    '\n')
+
+configuration = str()
+
+where_to_start_text = str(
+    F'# {where_to_start_header}\n'
+    '\n'
+    F'If you are a new user who is planning to use this program read about its [limitations]({limitations_link}) first.\n'
+    '\n'
+    F'Then go to [{install_header}]({install_link})\n'
+    '\n'
+    F'Then go to [{usage_header}]({usage_link})\n'
+    '\n'
+    F'If you want to understand the decisions that lead to this mess go to [{why_filenames_header}]({why_filenames_link})\n'
+    '\n'
+    F'If you think that putting the tags into the filenames is a bad solution go to [{alternatives_header}]({alternatives_link})\n'
+    '\n'
+)
+
+limitations_text = str(
+    F'# {limitations_header}\n'
+    '\n'
+    'Putting the tags into the filenames is limited by the maximum path length and maximum file length.\n'
+    'Windows, Linux, GoogleDrive, OneDrive, Dropbox, etc. all have different restraints\n'
+    'on those.\n'
+    '\n'
+    '- Linux has a maximum filename length of 255 characters for most filesystems (including EXT4), and a maximum path of\n'
+    '  4096\n'
+    '  characters. [link](https://unix.stackexchange.com/questions/32795/what-is-the-maximum-allowed-filename-and-folder-size-with-ecryptfs)\n'
+    '- Without Long Paths enabled Windows 10 has a maximum path length of\n'
+    '  260 [link](https://docs.microsoft.com/en-us/windows/win32/fileio/maximum-file-path-limitation?tabs=registry)\n'
+    '- With Long Paths enabled Windows 10 has a maximum path length of\n'
+    '  32,767 [link](https://docs.microsoft.com/en-us/windows/win32/fileio/maximum-file-path-limitation?tabs=registry#:~:text=path%20length%20of-,32%2C767%20characters,-.%20This%20type%20of)\n'
+    '\n'
+    'If you intend to use this tool you should check what the systems you are planning to use\n'
+    'are supporting. Maybe even run some tests.\n'
+    '\n'
+    'Make yourself clear about these limitations before using this tool as they cannot be circumvented.\n'
+    '\n'
+    'That said in most cases it´s pretty easy to stay below the limits unless you use very high number of long tags.\n'
+    '\n'
+)
+
+why_filenames_text = str(
+    F'# {why_filenames_header}?\n'
+    '\n'
+    'This is the conclusion of a [reddit post](TODO)\n'
+    '\n'
+    '# Summary\n'
+    '\n'
+    '- Filenames are a good enough solution in the majority of cases.\n'
+    '- Every good enough solution is better than a perfect solution that is never finished. (It has already taken me too long to implement this)\n'
+    '- Every alternative to filenames I found so far either had other limitations or would require 100s of hours of work\n'
+    '  to produce a satisfying program.\n'
+    '- I don\'t get paid so can\'t spend 100s of hours implementing niche tools that only a handful of people will use.\n'
+    '\n'
+    '## Why Filenames are Good\n'
+    '\n'
+    '- Very verbose, can see tags with every file browser\n'
+    '- Re-using what already exists\n'
+    '    - File browser\n'
+    '    - Thumbnail cache\n'
+    '    - Search and indexing functionality\n'
+    '    - Image viewers\n'
+    '- Works in pretty much every cloud web interface\n'
+    '    - => Access your media files via tags everywhere without having to download them first\n'
+    '- Works with every type of file\n'
+    '- Tags don`t get lost on conversion\n'
+    '- Tags don`t get lost when file is moved outside the tool\n'
+    '- Tags always stay synced with file even when working on multiple machines \n'
+    '  - => No need to sync databases, metafiles etc.\n'
+    '\n'
+    '## Alternative Solutions\n'
+    '\n'
+    '### Database\n'
+    '\n'
+    '- What happens when files are moved outside the tool?\n'
+    '- Would require indexing\n'
+    '- Needs a thumbnail cache and a file browser with search functionality\n'
+    '- How to sync the database to different PC?\n'
+    '- Can`t use tags in the cloud web interface\n'
+    '- Would basically just re-implement one of the countless tagging solutions that already exist\n'
+    '\n'
+    '### Accompanying Metafiles\n'
+    '\n'
+    '- Files always have to be moved together\n'
+    '- Would require indexing\n'
+    '- Needs a thumbnail cache and a file browser with search functionality\n'
+    '- Can`t use tags in the cloud web interface\n'
+    '\n'
+    '### Metadata of the Files\n'
+    '\n'
+    '- Not supported for a lot of file types\n'
+    '- Would need to treat every file type special\n'
+    '- Would require indexing\n'
+    '- Needs a thumbnail cache and a file browser with search functionality\n'
+    '- Can`t use tags in the cloud web interface\n'
+    '\n'
+)
+
+alternatives_text = str(
+    F'# {alternatives_header}\n'
+    '\n'
+    'Originally I was going to put a list of alternatives here, but then I found too many and none really satisfied me. That why I created this tool in the first place.\n'
+    '\n'
+    'There is an abundance of tagging tools out there. If you find one that is \n'
+    '- cross-platform\n'
+    '- works with cloud sync\n'
+    '- works in the cloud web interface\n'
+    '- can easily manage 100s of tags\n'
+    '- can easily manage dozens of tags per file  \n'
+    '\n'
+    'let me know, so I can add it.'
+)
+
+where_is_my_stuff_text = str(
+    F'# {where_is_my_stuff_header}?\n'
+    F'\n'
+    F'1. Local config dir : [{__paths.local_config_dir()}](file://{__paths.local_config_dir()})\n'
+    F'2. User config dir : [{__paths.user_config_dir()}](file://{__paths.user_config_dir()})\n'
+    F'3. System config dir : [{__paths.system_config_dir()}](file://{__paths.system_config_dir()})\n'
+)
+
+install_text = str(
+    F'# {install_header}\n'
+    F'TODO'
+)
+
+usage_text = str(
+    F'# {usage_header}\n'
+    F'TODO'
+)
+
+
+class Section:
+    def __init__(self,
+                 link,
+                 header,
+                 text):
+        self.link = link
+        self.header = header
+        self.text = text
+
+
+sections = [Section(about_link,
+                    about_header,
+                    about_text),
+            Section(where_to_start_link,
+                    where_to_start_header,
+                    where_to_start_text),
+            Section(limitations_link,
+                    limitations_header,
+                    limitations_text),
+            Section(why_filenames_link,
+                    why_filenames_header,
+                    why_filenames_text),
+            Section(alternatives_link,
+                    alternatives_header,
+                    alternatives_text),
+            Section(where_is_my_stuff_link,
+                    where_is_my_stuff_header,
+                    where_is_my_stuff_text),
+            Section(install_link,
+                    install_header,
+                    install_text),
+            Section(usage_link,
+                    usage_header,
+                    usage_text)]
+
+sections_by_link: Dict[str, Section] = {section.link: section for section in sections}
+
+
+def __build_readme_table_of_contents__() -> str:
+    sections_copy = sections.copy()
+
+    sections_copy.remove(sections_by_link[where_is_my_stuff_link])
+
+    return __build_table_of_contents__(sections_copy)
+
+
+def __build_table_of_contents__(sections_for_table: List[Section]) -> str:
+    i = 1
+
+    entries = []
+
+    for section in sections_for_table:
+        entries.append(F'{i}. [{section.header}]({section.link})')
+
+    return "\n".join(entries)
+
+
+readme_table_of_contents = __build_readme_table_of_contents__()
+
+help_table_of_contents = __build_table_of_contents__(sections)
+
+help_text = '\n\n'.join([about_text,
+                         where_to_start_text,
+                         limitations_text,
+                         why_filenames_text,
+                         alternatives_text])
--- src/ftg/view/help/help_dialog.py
+++ src/ftg/view/help/help_dialog.py
@@ -1,10 +1,15 @@
+import logging
+import os.path
+import traceback
+import urllib.request
 import webbrowser
 from tkinter import Tk, BOTH, Toplevel
+from typing import Callable
 
 import markdown
 from tkinterweb.htmlwidgets import HtmlFrame
 
-from ftg.__help_text import help_text
+from ftg.__help_text import help_table_of_contents, sections, sections_by_link
 from ftg.view.styles import Styles
 from ftg.view.ui_config import UIConfig
 
@@ -23,22 +28,90 @@ class FtgHelpDialog:
 
         self.__build_ui()
 
+        self.__show_markdown(help_table_of_contents)
+
     def __build_ui(self):
-        text = HtmlFrame(self.__root,
-                         messages_enabled=False,
-                         vertical_scrollbar="auto",
-                         horizontal_scrollbar="auto")
+        self.html_frame = HtmlFrame(self.__root,
+                                    messages_enabled=False,
+                                    vertical_scrollbar="auto",
+                                    horizontal_scrollbar="auto")
 
-        def open_url(url:str):
-            if url.startswith("https://"):
-                webbrowser.open(url)
+        def open_url(url: str):
+            self.__open_url(url)
 
-        text.html.link_click_func = open_url
-        text.html.form_submit_func = lambda: None
+        self.html_frame.html.link_click_func = open_url
+        self.html_frame.html.form_submit_func = lambda: None
 
-        text.pack(fill=BOTH,
-                  expand=True)
+        self.html_frame.pack(fill=BOTH,
+                             expand=True)
 
-        html_help_text = markdown.markdown(help_text)
+    def __show_markdown(self,
+                        markdown_text):
 
-        text.load_html(html_help_text)
+        html_text = markdown.markdown(markdown_text)
+
+        self.html_frame.load_html(html_text)
+
+    # tkinterweb just swallows exceptions...
+    def __wrap_with_logger(self,
+                           fun: Callable):
+
+        try:
+            fun()
+        except Exception:
+            logging.warning(traceback.format_exc())
+
+    def __open_url(self,
+                   url):
+
+        self.__wrap_with_logger(lambda: self.__try_open_url(url))
+
+    def __try_open_url(self,
+                       url):
+
+        if url.startswith("https://"):
+            webbrowser.open(url)
+            return
+
+        if url.startswith("file://"):
+
+            filename = url.split("/")[-1]
+
+            if filename.startswith("#"):
+
+                if filename == "#home":
+                    self.__show_markdown(help_table_of_contents)
+                    return
+
+                if filename in sections_by_link.keys():
+
+                    section = sections_by_link[filename]
+
+                    section_markdown_text = section.text
+
+                    navigation_links = "[Home](#home)"
+
+                    index = sections.index(section)
+
+                    if index > 0:
+                        navigation_links += F'  [Previous]({sections[index - 1].link})'
+
+                    if index < len(sections) - 1:
+                        navigation_links += F'  [Next]({sections[index + 1].link})'
+
+                    self.__show_markdown(navigation_links + "\n\n" + section_markdown_text)
+                return
+
+            try:
+                path = urllib.request.url2pathname(url)
+            except OSError as ex:
+                # urls aren't conforming
+                # get rid of file://
+                path = url[7:]
+
+            if os.path.exists(path):
+                if os.path.isdir(path):
+                    os.startfile(path)
+                elif os.path.isfile(path):
+                    os.startfile(os.path.dirname(path))
+                return
