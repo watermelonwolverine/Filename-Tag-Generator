9bab5eac7838ca7e560b89da3e48f782be8cf6da
watermelonwolverine <29666253+watermelonwolverine@users.noreply.github.com>
2022-08-04 16:07:37 +0200

    Removed upx from build. Made absolutely no difference in the end. Neither for false positives on VirusTotal, nor for size.

--- README.md
+++ README.md
@@ -367,11 +367,8 @@ This section assumes you have extensive programming and Python experience as I w
 If you want to build the program yourself, these are the steps:
 
 1. Clone repository
-2. Install requirements (see requirements.txt)
-3. On Windows ..
-    1. .. EITHER download [UPX](https://upx.github.io/) and extract it into a folder called `upx` next to the project directory.
-    2. .. OR remove the `--upx-dir ..\upx` part from any build_for_windows_*_console.ps1 script
-4. Build
+2. Install requirements. Can usually be done with `pip install -r requirements.txt`)
+3. Build
     1. On Windows run one of the build_for_windows_*_console.ps1 files
     2. On Ubuntu run one of the build_for_ubuntu_*_console.sh files
 
--- src/ftg/__help/build.py
+++ src/ftg/__help/build.py
@@ -10,11 +10,8 @@ text = str(
     F'If you want to build the program yourself, these are the steps:\n'
     F'\n'
     F'1. Clone repository\n'
-    F'2. Install requirements (see requirements.txt)\n'
-    F'3. On Windows ..\n'
-    F'    1. .. EITHER download [UPX](https://upx.github.io/) and extract it into a folder called `upx` next to the project directory.\n'
-    F'    2. .. OR remove the `--upx-dir ..\\upx` part from any build_for_windows_*_console.ps1 script\n'
-    F'4. Build\n'
+    F'2. Install requirements. Can usually be done with `pip install -r requirements.txt`)\n'
+    F'3. Build\n'
     F'    1. On Windows run one of the build_for_windows_*_console.ps1 files\n'
     F'    2. On Ubuntu run one of the build_for_ubuntu_*_console.sh files'
 )
